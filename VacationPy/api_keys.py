e9289c4d88f1bc6303018880ee98c681498e2d5d
sparking29 <sparking29@gmail.com>
2020-01-16 16:58:03 -0600

    VacationPy complete

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "8d94d91c527baf768c353a0b929589a9"
+weather_api_key = "YOURS HERE"
 
 # Google API Key
-g_key = "AIzaSyDDjZUgDxTCQFq_sRJDF1_DXz7G3X-1o6Q"
+g_key = "YOURS HERE"
